0664c0b98ef034237ffdf027b55353ed45032e47
frankemesinwa <frankemesinwa@gmail.com>
2025-08-25 00:07:58 +0000

    new file:   backend/prisma/migrations/20250825000511_admin_auth_init/migration.sql 	modified:   backend/prisma/schema.prisma

---
+++ backend/prisma/migrations/20250825000511_admin_auth_init/migration.sql
@@ -0,0 +1,25 @@
+-- CreateEnum
+CREATE TYPE "AdminRole" AS ENUM ('ADMIN', 'SUPERADMIN');
+
+-- CreateTable
+CREATE TABLE "Admin" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "role" "AdminRole" NOT NULL DEFAULT 'ADMIN',
+    "is2FAEnabled" BOOLEAN NOT NULL DEFAULT false,
+    "totp_secret" TEXT,
+    "failedLoginAttempts" INTEGER NOT NULL DEFAULT 0,
+    "lockedUntil" TIMESTAMP(3),
+    "lastLoginAt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Admin_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Admin_email_key" ON "Admin"("email");
+
+-- CreateIndex
+CREATE INDEX "Admin_email_idx" ON "Admin"("email");
